e20419a73340c3aae0eb79dbbd13a90177892e10
soapie18 <168833604+soapie18@users.noreply.github.com>
2026-05-13 18:38:28 +0100

    Stacking system finished

--- Scripts/component_base.gd
+++ Scripts/component_base.gd
@@ -3,8 +3,7 @@ class_name ComponentBase extends CharacterBody2D
 @export var is_falling: bool = true
 @export var falling_speed: float = 4000
 
-func _fall_downwards(delta) -> void:
-	#Change this to move_and_slide() based movement so it doesnt crash against the plate. 
+func _fall_downwards(delta) -> void: 
 	velocity.y = falling_speed * delta
 	move_and_slide()
 
@@ -12,4 +11,3 @@ func _fall_downwards(delta) -> void:
 func _process(delta: float) -> void:
 	if is_falling:
 		_fall_downwards(delta)
-	
--- Scripts/player_stack.gd
+++ Scripts/player_stack.gd
@@ -21,18 +21,26 @@ func _process(_delta: float) -> void:
 	
 	velocity.y = 0
 	
+	#Raising the Area2D to be able to detect the next object falling.
+	area2D.global_position = current_stack_point.global_position
+	
 	move_and_slide()
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	#Reparenting the falling componenet to be a child of the plate.
 	if body is ComponentBase:
 		body.is_falling = false
+		body.set_deferred("monitorable", false)
 		body.global_position = current_stack_point.global_position
-		body.reparent(self)
+		#Reparenting the falling componenet to be a child of the plate.
+		body.call_deferred("reparent", self)
+	
+		#Stopping further collisions for this component.
+		var collider = body.get_node("CollisionShape2D")
+		body.remove_child(collider)
+		
+	
 		current_stack.append(body)
 		current_stack_point = current_stack.back().get_node("StackPoint")
 		
-		#Raising the Area2D to be able to detect the next object falling.
-		area2D.global_position = current_stack_point.global_position
-		print(area2D.global_position)
+		print(current_stack)
